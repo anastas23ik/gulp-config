233b9cc1ef080196e5a28cf40e936ed403e2be14
anastas23ik <23askomorebi@gmail.com>
2024-07-22 22:50:45 +0500

    added html build task and css build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,19 @@
 const gulp = require('gulp');
+const fileInclude = require('gulp-file-include');
+const sass = require('gulp-sass')(require('sass'))
 
-gulp.task('hello', function(done){
-  console.log('Hello, from Gulp');
-
-  done();
+const fileIncludeSetting = {
+  prefix: '@@',
+  basepath: '@file'
+};
+gulp.task('includeFiles', function() {
+  return gulp.src('./src/*.html')
+    .pipe(fileInclude(fileIncludeSetting))
+    .pipe(gulp.dest('./dist'))
 });
 
-gulp.task('default', gulp.series('hello'));
-
-// gulp.task('default', function() {
-//   gulp.series('hello')
-// });
+gulp.task('sass',function() {
+  return gulp.src('./src/scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./dist/css/'))
+})
